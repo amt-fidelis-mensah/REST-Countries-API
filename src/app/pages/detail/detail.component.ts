bd4cc30ab009d6ba49106726c16c943e0a1ffe84
Fidelis Mensah <fidelis.mensah@amalitech.org>
2023-10-03 09:21:49 +0000

    [object Object] error solved

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -2,14 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, mergeMap, of, tap } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
-import {
-  Country,
-  NativeName,
-  Currencies,
-  Languages,
-  Name,
-  Translation,
-} from 'src/app/types/api';
+import { Country } from 'src/app/types/api';
 
 @Component({
   selector: 'app-detail',
@@ -34,14 +27,24 @@ export class DetailComponent implements OnInit {
     });
   }
 
-  displayNativeName(nativeName: Name[]) {
-    return nativeName.map((name) => name.common).join(', ');
+  displayNativeName(nativeName: {
+    [key: string]: { official: string; common: string };
+  }) {
+    return Object.values(nativeName)
+      .map((item) => item.common)
+      .join(', ');
   }
 
-  displayCurrencies(currencies: Currencies[]) {
-    return currencies.map((currency) => currency.COP.name).join(', ');
+  displayCurrencies(currencies: {
+    [key: string]: { name: string; symbol: string };
+  }) {
+    return Object.values(currencies)
+      .map((item) => item.name)
+      .join(', ');
   }
-  displayLanguages(languages: Languages[]) {
-    return languages.map((language) => language.spa).join(', ');
+  displayLanguages(languages: { [key: string]: string }) {
+    return Object.values(languages)
+      .map((item) => item)
+      .join(', ');
   }
 }
